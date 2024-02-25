ece63ed1c274e2beb10de614b7d84dc9de1c271e
DiegoYC <diegoyc@outlook.com>
2024-02-24 22:33:48 -0500

    CORS: Hotfix

--- src/main/java/com/evertec/springboot2/crud/controller/TareaController.java
+++ src/main/java/com/evertec/springboot2/crud/controller/TareaController.java
@@ -26,6 +26,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "https://evertec-frontend.vercel.app")
 @Api(tags = "Tareas", description = "Operaciones relacionadas con el CRUD tareas.")
 public class TareaController {
 	@Autowired
